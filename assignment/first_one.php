fe6ba0b3f75bd0130ced887f554219308d5c36f7
AtiqBabu <ar.diu.cse@gmail.com>
2020-08-08 23:09:16 +0600

    This is class-01 lecture code

---
+++ assignment/first_one.php
@@ -0,0 +1,19 @@
+<?php
+
+    echo "Simple Calculation<br><br>";
+
+    $a = 7;
+    $b = 3;
+
+    echo "Two Numbers are: a = ".$a. " b = ".$b. "<br><br>"; 
+
+    echo "Sum = ".($a+$b)."<br>";
+    echo "Sub = ".($a-$b)."<br>";
+    echo "Mult = ".($a*$b)."<br>";
+    echo "Div = ".round(($a/$b), 2)."<br>";
+    echo "Mod = ".($a%$b)."<br>";
+
+    echo " <br>This is build by Md \"Atiqur Rahman\"";
+
+
+?>
